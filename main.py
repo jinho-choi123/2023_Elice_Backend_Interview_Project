86ee772cbcaab477024f7431ca3063a5329acd8c
Jinho-Choi123 <choijinho817@gmail.com>
2023-11-23 19:52:53 +0900

    setup: add python dotenv to manage secrets

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+import os 
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv(os.path.join(BASE_DIR, ".config", ".env"))
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {
-        "message": "Hello World"
+        "message": "Hello world"
     }
