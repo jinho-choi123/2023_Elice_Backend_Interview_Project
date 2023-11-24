296fe9ddd5c803f5b31d11d2fb27f2bdbdf33a87
Jinho-Choi123 <choijinho817@gmail.com>
2023-11-24 11:53:47 +0900

    feat: implement user signin controller

--- src/controller/authController.py
+++ src/controller/authController.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy.orm import Session 
 from sqlalchemy import select
 from src.db import database, models
-from src.types.authTypes import authRequest, authResponse
+from src.types.authTypes import authSigninRequest, authSignupRequest
 from src.types.userTypes import userCreation
-from src.utils.hash import generate_rand_salt, hash_password
+from src.utils.hash import check_password_match, generate_rand_salt, hash_password
 from sqlalchemy.exc import SQLAlchemyError
+from src.db.database import redis_client
+from src.utils.session import generate_cookie
 
 # return True if user does not exists
 # return False o/w
@@ -15,9 +18,17 @@ def check_user_exists(db: Session, email: str):
         return False
     else:
         return True
+
+def get_user_id(db: Session, email: str):
+    stmt = select(models.User).where(models.User.email == email)
+    result = db.scalar(stmt)
+    if(not result):
+        return None 
+    else:
+        return result.id
     
 # create user object 
-def user_signup(db: Session, signupForm: authRequest):
+def user_signup(db: Session, signupForm: authSignupRequest):
     try:
         # generate user specific salt 
         hash_salt = generate_rand_salt(256)
@@ -42,3 +53,30 @@ def user_signup(db: Session, signupForm: authRequest):
     except SQLAlchemyError as e:
         print(str(e.orig))
         return False
+
+def user_signin(db: Session, signinForm: authSigninRequest):
+    # check if user is valid 
+    # assume that user record with given email exists
+    SESSION_EXP_TIME = int(os.environ["SESSION_EXP_TIME"])
+
+    ## get password hash from db
+    stmt = select(models.User).where(models.User.id == 1)
+    result = db.scalar(stmt)
+
+    ## must not happen 
+    if(not result):
+        raise Exception("user with given email does not exist")
+    
+    stored_passwordHash = result.password_hash
+    password_salt = result.password_salt
+
+    if(check_password_match(signinForm.password, stored_passwordHash, password_salt)):
+        ## login success!
+        ## create cookie and add it to redis 
+        user_id = get_user_id(db, signinForm.email)
+        cookie = generate_cookie()
+        redis_client.set(cookie, user_id, ex=SESSION_EXP_TIME)
+        return cookie 
+    else:
+        ## login Failed!!
+        return None
