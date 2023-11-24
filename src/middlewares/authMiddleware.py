a283ab4d30d7553141dbb75858f53fabba92698e
Jinho-Choi123 <choijinho817@gmail.com>
2023-11-25 03:11:05 +0900

    fix: fix bug in auth middleware

--- src/middlewares/authMiddleware.py
+++ src/middlewares/authMiddleware.py
@@ -5,20 +5,22 @@ from src.db.database import SessionLocal, redis_client
 
 def get_current_user(cookie):
     # handle if cookie is None
-    if(not cookie):
+    if not cookie:
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Cookie does not exists",
         )
     
     # get user id from cookie value 
-    user_id = int(redis_client.get(cookie))
+    user_id = redis_client.get(cookie)
 
     if not user_id:
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Invalid cookie value",
         )
+    
+    user_id = int(user_id)
     
     db = SessionLocal()
     user_session = get_user_session(db, user_id)
