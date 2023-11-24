2dcf683d8bfe8eb52e2d2742c0ca6881763fe8ab
Jinho-Choi123 <choijinho817@gmail.com>
2023-11-25 03:10:31 +0900

    feat: implement board routers. use modern sqlalchemy methods

--- src/controller/boardController.py
+++ src/controller/boardController.py
@@ -1,66 +1,52 @@
+from itertools import count
 from fastapi import HTTPException, status
-import psycopg2
 from sqlalchemy.orm import Session
 
 from sqlalchemy.exc import SQLAlchemyError
 from src.types.userTypes import userSession 
-from sqlalchemy import select
+from sqlalchemy import asc, delete, desc, select, func, update
 from src.db import models, database
-from src.types.boardTypes import boardBaseRequest, boardDeletion, boardGet, boardGetResponse, boardUpdate
+from src.types.boardTypes import boardBaseRequest, boardDeletion, boardGet, boardObj, boardPagination, boardUpdate
+import math
 
 ## check if current user is board's owner
 def is_board_owner(board_id: int, userSession: userSession):
+    owner_boards_ids = list(map(lambda board: board.id, userSession.boards))
     # just check if userSession's board include board_id 
-    if board_id in userSession.boards:
+    if board_id in owner_boards_ids:
         # current user is board's owner 
         return True 
     else:
         return False
-    
-def check_board_name_exists(db: Session, board_name: str):
-    stmt = select(models.Board).where(models.Board.name == board_name)
-    result = db.scalar(stmt)
-    if(result):
-        return True 
+
+def get_board_by_id(db: Session, board_id: int)->boardObj:
+    stmt = select(models.Board).where(models.Board.id == board_id)
+    result = db.execute(stmt).fetchone()
+    if not result:
+        return None 
     else:
-        return False
+        returnObj = boardObj(id = result[0].id, name = result[0].name, isPublic = result[0].isPublic, posts = result[0].posts, creator_id = result[0].creator_id)
+        return returnObj
+
+def get_board_by_name(db: Session, board_name: str)->boardObj:
+    stmt = select(models.Board).where(models.Board.name == board_name)
+    result = db.execute(stmt).fetchone()
+    if not result:
+        return None 
+    else:
+        returnObj = boardObj(id = result[0].id, name = result[0].name, isPublic = result[0].isPublic, posts = result[0].posts, creator_id = result[0].creator_id)
+        return returnObj
 
 # check board name exists except the given board id
 def check_board_name_exists_except(db: Session, board_name: str, board_id: int):
-    stmt = select(models.Board).where(models.Board.id != board_id).where(models.Board.name == board_name)
-    result = db.scalar(stmt)
-    
-    if(result):
-        return True 
-    else:
+    same_name_board = get_board_by_name(db, board_name)
+    if not same_name_board:
+        return False 
+    if same_name_board.id == board_id:
         return False
-
-def check_board_id_exists(db: Session, board_id: int):
-    stmt = select(models.Board).where(models.Board.id == board_id)
-    result = db.scalar(stmt)
-
-    if(result):
-        return True 
     else:
-        return False
+        return True
 
-def is_board_viewable(db: Session, user_session: userSession, board_id: int):
-    stmt = select(models.Board).where(models.Board.id == board_id)
-    result = db.scalar(stmt)
-
-    # if board does not exists
-    if not result:
-        return False, None 
-    
-    is_Viewable = result.isPublic
-
-    ## if user is board owner, then board is viewable
-    if is_board_owner(board_id, user_session):
-        is_Viewable = True
-    
-    return is_Viewable, result
-    
-    
 def board_create(db: Session, boardForm: boardBaseRequest, user_session: userSession):
     try:
         board_creation = boardForm
@@ -79,38 +65,85 @@ def board_create(db: Session, boardForm: boardBaseRequest, user_session: userSes
         )
     
 def board_update(db: Session, boardForm: boardUpdate):
-    stmt = select(models.Board).where(models.Board.id == boardForm.id)
-    updating_board = db.scalar(stmt)
-
-    # will not reach due to is_board_owner function
-    if not updating_board:
-        # board with given name and id does not exist
-        return False 
-    
-    updating_board.name = boardForm.name 
-    updating_board.isPublic = boardForm.isPublic
-    db.commit()
-    return True
+    try:
+        stmt = (
+            update(models.Board)
+                .where(models.Board.id == boardForm.id)
+                .values(
+                    {
+                        models.Board.name: boardForm.name,
+                        models.Board.isPublic: boardForm.isPublic
+                    }
+                )
+        )
+        db.execute(stmt)
+        db.commit()
+        return True
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code = status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail = str(e.orig)
+        )
 
 
 def board_delete(db: Session, boardForm: boardDeletion):
-    stmt = select(models.Board).where(models.Board.id == boardForm.id)
-    deleting_board = db.scalar(stmt)
-
-    # will not reach due to is_board_owner function
-    if not deleting_board:
-        return False
-    
-    db.delete(deleting_board)
+    stmt = delete(models.Board).where(models.Board.id == boardForm.id)
+    db.execute(stmt)
     db.commit()
     return True
 
 def board_get(db: Session, user_session: userSession, boardForm: boardGet):
-    isViewable, board = is_board_viewable(db, user_session, boardForm.id)
+    board = get_board_by_id(db, boardForm.id)
 
-    if not isViewable:
-        return None 
+    # check if current user is board's owner 
+    if board.creator_id == user_session.id or board.isPublic == True:
+        return board
+
+    return None
+
+def boards_pagination(db: Session, board_pagination: boardPagination, total_boards_size: int, user_session: userSession):
+    pageNum = board_pagination.page
+    pageSize = board_pagination.pageSize
+    # if pageNum <= 0 then set pageNum to 1
+    if pageNum <= 0:
+        pageNum = 1
+
+    lastPageNum = math.ceil(total_boards_size / pageSize)
+
+    # if pageNum exceeds boards_size/pageSize, then return end page
+    if pageNum > lastPageNum:
+        pageNum = lastPageNum
+
+    # calculate offset and limit
+    offset = (pageNum - 1) * pageSize
+    limit = pageSize
+
+    # query boards 
+    # order by desc order of # of posts in board
+    stmt = select(models.Board, func.count(models.Post.id).label("count")) \
+        .outerjoin(models.Post) \
+        .where((models.Board.creator_id == user_session.id) | (models.Board.isPublic == True)) \
+        .group_by(models.Board.id) \
+        .order_by(desc("count")) \
+        .offset(offset) \
+        .limit(limit)
     
-    boardInfo = boardGetResponse(id = board.id, name = board.name, isPublic = board.isPublic, posts = board.posts)
+    boards_list = db.execute(stmt).fetchall()
+    boards_result = list()
 
-    return boardInfo
+    for board in list(boards_list):
+        boards_result.append(
+            boardObj(
+                id = board[0].id,
+                name = board[0].name,
+                isPublic = board[0].isPublic,
+                posts = board[0].posts,
+                creator_id = board[0].creator_id
+            )
+        )
+
+    return boards_result
+
+def total_boards_size(db: Session):
+    boardSize = db.query(models.Board).count()
+    return boardSize
--- src/routers/boardRouter.py
+++ src/routers/boardRouter.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Cookie, Response, status
+from src.controller.boardController import board_create, board_delete, board_update, boards_pagination, check_board_name_exists_except, get_board_by_id, get_board_by_name, is_board_owner, total_boards_size
 from src.db.database import SessionLocal
-from src.controller.boardController import board_create, board_update, check_board_name_exists, check_board_name_exists_except, is_board_owner
 
 from src.middlewares.authMiddleware import get_current_user
-from src.types.boardTypes import boardBaseRequest, boardResponse, boardUpdate
+from src.types.boardTypes import boardBaseRequest, boardDeletion, boardListResponse, boardObjResponse, boardPagination, boardResponse, boardUpdate
 
 boardRouter = APIRouter(
     prefix="/board",
@@ -15,18 +15,22 @@ def board_Create(boardForm: boardBaseRequest, response: Response, session_id: st
     db = SessionLocal()
 
     # check if board name is already occupied 
-    if check_board_name_exists(db, boardForm.name):
+    if get_board_by_name(db, boardForm.name):
         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        return boardResponse(
+        return boardObjResponse(
             success = False,
             message = "Board name already in use",
+            board = None
         )
 
     # create board
     if board_create(db, boardForm, user):
-        return boardResponse(
+        # get board object from db 
+        newBoard = get_board_by_name(db, boardForm.name)
+        return boardObjResponse(
             success = True,
-            message = "Board Creation Success!"
+            message = "Board Creation Success!",
+            board = newBoard
         )
 
 @boardRouter.patch("/{board_id}")
@@ -35,7 +39,7 @@ def board_Update(board_id: int, boardForm: boardBaseRequest, session_id: str | N
     db = SessionLocal()
 
     ## check if current user is board owner 
-    if is_board_owner(board_id, user):
+    if not is_board_owner(board_id, user):
         return boardResponse(
             success = False,
             message = "LoggedIn user is not board owner."
@@ -61,3 +65,77 @@ def board_Update(board_id: int, boardForm: boardBaseRequest, session_id: str | N
             success = False, 
             message = "Board update failed. Please try again."
         )
+
+@boardRouter.delete("/{board_id}")
+def board_Delete(board_id: int, session_id: str | None = Cookie(default=None)):
+    user = get_current_user(session_id)
+    db = SessionLocal()
+
+    if not get_board_by_id(db, board_id):
+        return boardResponse(
+            success = False,
+            message = "Board does not exists."
+        )
+
+    ## check if current user is board owner 
+    if not is_board_owner(board_id, user):
+        return boardResponse(
+            success = False,
+            message = "LoggedIn user is not board owner."
+        )
+    
+    boardForm = boardDeletion(
+        id = board_id
+    )
+    
+    board_delete(db, boardForm)
+
+    return boardResponse(
+        success = True,
+        message = "Board deletion success!"
+    )
+
+# should be front of get /. 
+# else, this endpoint wouldnt be reached
+@boardRouter.get("/list")
+def board_List(page: int = 0, pageSize: int = 10, session_id: str | None = Cookie(default=None)):
+    user = get_current_user(session_id)
+
+    board_pagination = boardPagination(page = page, pageSize = pageSize)
+    db = SessionLocal()
+    total_boards = total_boards_size(db)
+    showing_boards = boards_pagination(db, board_pagination, total_boards, user)
+    return boardListResponse(
+        success = True,
+        message = "Board List API",
+        boards = showing_boards
+    )
+
+@boardRouter.get("/{board_id}")
+def board_get(board_id: int, session_id: str | None = Cookie(default=None)):
+    user = get_current_user(session_id)
+    db = SessionLocal()
+    board = get_board_by_id(db, board_id)
+
+    if not board:
+        return boardObjResponse(
+            success = False,
+            message = "Board does not exists",
+            board = None
+        )
+
+    if is_board_owner(board_id, user) or board.isPublic == True:
+        return boardObjResponse(
+            success = True,
+            message = "Board get success",
+            board = board
+        )
+    else:
+        return boardObjResponse(
+            success = False,
+            message = "Board get failed",
+            board = None
+        )
+
+
+
