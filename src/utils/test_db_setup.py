edb05cf1d169ded3eb778bded51aa953b60dcdc7
Jinho-Choi123 <choijinho817@gmail.com>
2023-11-26 20:55:21 +0900

    refactor: modulize test db setup

---
+++ src/utils/test_db_setup.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.db.database import Base
+
+# Get Test DB URL
+TEST_DB_URL = os.environ["POSTGRES_TEST_DB_URL"]
+
+engine = create_engine(TEST_DB_URL)
+
+TEST_SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    try: 
+        db = TEST_SessionLocal()
+        yield db 
+    finally:
+        db.close()
+
+@pytest.fixture()
+def refresh_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
